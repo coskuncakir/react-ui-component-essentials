80ac4f7e07a3d3e88bdf8fc429010eaf3cfcfe7d
Coskun Cakir <coskuncakirr@gmail.com>
2024-02-01 09:24:17 +0000

    button component

--- stories/Button.tsx
+++ stories/Button.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
 import "./button.css";
 
-/**
- * Primary UI component for user interaction
- */
 export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
   const mode = primary
     ? "storybook-button--primary"
@@ -21,34 +17,4 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
       {label}
     </button>
   );
-};
-
-Button.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
-  size: PropTypes.oneOf(["small", "medium", "large"]),
-  /**
-   * Button contents
-   */
-  label: PropTypes.string.isRequired,
-  /**
-   * Optional click handler
-   */
-  onClick: PropTypes.func,
-};
-
-Button.defaultProps = {
-  backgroundColor: null,
-  primary: false,
-  size: "medium",
-  onClick: undefined,
 };
